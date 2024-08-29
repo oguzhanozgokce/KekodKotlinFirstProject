84580e5e673ef848ce926424306902c558ed2159
Oguzhan <ozgokceoguzhan34@gmail.com>
2024-08-29 03:46:36 +0300

    - String.xlm, existing strings moved there - fix: remove unnecessary empty import

--- app/src/main/java/com/oguzhanozgokce/kekodkotlinfirstproject/ui/ego/EgoFragment.kt
+++ app/src/main/java/com/oguzhanozgokce/kekodkotlinfirstproject/ui/ego/EgoFragment.kt
@@ -1,23 +1,15 @@
 package com.oguzhanozgokce.kekodkotlinfirstproject.ui.ego
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.switchmaterial.SwitchMaterial
-import com.oguzhanozgokce.kekodkotlinfirstproject.MainActivity
 import com.oguzhanozgokce.kekodkotlinfirstproject.R
-import com.oguzhanozgokce.kekodkotlinfirstproject.common.gone
 import com.oguzhanozgokce.kekodkotlinfirstproject.databinding.FragmentEgoBinding
-import com.oguzhanozgokce.kekodkotlinfirstproject.common.setEnabled
-import com.oguzhanozgokce.kekodkotlinfirstproject.common.visible
 
 class EgoFragment : Fragment() {
     private var _binding: FragmentEgoBinding? = null
